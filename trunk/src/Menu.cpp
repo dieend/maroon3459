2318ab21c0ad4137cb059b27f12a1b1a61dd22ab
mail.dieend@gmail.com <mail.dieend@gmail.com@90a5ba63-80d7-4a8f-ce7f-e41919d26129>
2012-02-22 04:43:53 +0000

    display_menu() completed

--- trunk/src/Menu.cpp
+++ trunk/src/Menu.cpp
@@ -16,6 +16,11 @@ void Menu::display_main_menu() const
 void Menu::display_info() const
 {
 	cout << "Here is the Info requested \n";//display Project Part I, team name and team members
+	cout << "Team name    : " << "Maroon-345-9\n";
+	cout << "Team members : " << "Muhammad Adinata\n";
+	cout << "               " << "Aldo Suwandi\n";
+	cout << "               " << "Selvin Perez\n";
+	cout << "\n";
 }
 
 void Menu::read()
