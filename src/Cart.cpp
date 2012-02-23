0704cb9fc74fe4eadf9deb9fcf9d7549f8ad9d6e
mail.dieend@gmail.com <mail.dieend@gmail.com@90a5ba63-80d7-4a8f-ce7f-e41919d26129>
2012-02-23 17:44:38 +0000

    Cart.cpp and Cart_item.cpp, sorry for forgetting to commit it

---
+++ src/Cart.cpp
@@ -0,0 +1,31 @@
+#include "Cart.h"
+
+Cart::Cart (int cart_id, int cust_id, Date& cpd){
+	this->cart_id = cart_id;
+	this->cust_id = cust_id;
+	this->cart_purchase_date = cpd;
+}
+Cart::Cart(){
+}
+// utility functions
+string Cart::display() const{
+	stringstream ss;
+	ss << "Cart id       : " << cart_id << endl;
+	ss << "Cust id       : " << cust_id << endl;
+	ss << "Purchase Date : " << cart_purchase_date << endl;
+	string s = ss.str();
+	return s;
+}
+ostream& operator<<(ostream& c, Cart d){
+	c << d.display();
+	return c;
+}
+istream& operator>>(istream& c, Cart& d){
+	int cart_id,cust_id;
+	Date date;
+	c >> cart_id >> cust_id >> date;
+	d = Cart(cart_id,cust_id,date);
+	return c;
+}
+
+
