58121a14a9490b13ecbd7440352823ec18a7840b
selvin713@gmail.com <selvin713@gmail.com@90a5ba63-80d7-4a8f-ce7f-e41919d26129>
2012-02-23 18:20:27 +0000

    updated #define

--- src/Address.h
+++ src/Address.h
@@ -1,12 +1,16 @@
+#ifndef ADDRESS_H_
+#define ADDRESS_H_
 #include <string>
 
 
 using namespace std;
 
 struct Address	{
+public:
 	int house_num;
 	string street_name;
 	int zip;
 	
 	Address (int n, string s, int z): house_num(n), street_name(s), zip(z){}
 };
+#endif //ADDRESS_H_
