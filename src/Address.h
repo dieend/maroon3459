e311e2145976c0bfba99781deee51dd0b0821afd
selvin713@gmail.com <selvin713@gmail.com@90a5ba63-80d7-4a8f-ce7f-e41919d26129>
2012-03-01 03:57:03 +0000

    added display()

--- src/Address.h
+++ src/Address.h
@@ -1,16 +1,25 @@
 #ifndef ADDRESS_H_
 #define ADDRESS_H_
 #include <string>
+#include <stdio.h>
 
 
 using namespace std;
 
-struct Address	{
+struct Address  {
 public:
-	int house_num;
-	string street_name;
-	int zip;
-	
-	Address (int n, string s, int z): house_num(n), street_name(s), zip(z){}
+        int house_num;
+        string street_name;
+        int zip;
+        
+        Address (int n, string s, int z): house_num(n), street_name(s), zip(z){}
+
+        string display() const{
+        char num_buff [50], zip_buff [50];
+        sprintf(num_buff,"%d", house_num);
+        sprintf(zip_buff,"%d", zip);
+        string num_string (num_buff), zip_string (zip_buff);
+        return num_string +" "+ street_name +" "+ zip_string;
+        }
 };
 #endif //ADDRESS_H_
