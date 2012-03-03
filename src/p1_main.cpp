018db85680b7eaeda3cb21c530926378bdbb1fe9
mail.dieend@gmail.com <mail.dieend@gmail.com@90a5ba63-80d7-4a8f-ce7f-e41919d26129>
2012-03-03 03:33:45 +0000

    turn off read test menu

--- src/p1_main.cpp
+++ src/p1_main.cpp
@@ -43,8 +43,8 @@ try {
          break;
 	  case Menu::Read:
          try {
-            //m.read();
-            m.read_test();
+            m.read();
+            //m.read_test();
          } catch (const Menu::InvalidFile& excp) {
             cerr << excp.what() << endl;
          } catch (const Menu::InvalidData& excp) {
