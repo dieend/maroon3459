4e70d6fdafca165d244204cd84932c68d5dafc3a
selvin713@gmail.com <selvin713@gmail.com@90a5ba63-80d7-4a8f-ce7f-e41919d26129>
2012-04-27 04:06:14 +0000

    c2 header

---
+++ OLES_GUI/GraphicsSP.h
@@ -0,0 +1,31 @@
+#ifndef GRAPHICSSP_H
+#define GRAPHICSSP_H
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include "Simple_window.h"
+#include <iostream>
+#include "Cart.h"
+#include "Cart_item.h"
+#include "Category.h"
+#include "Product.h"
+#include "Customer.h"
+
+
+using namespace std;
+
+	class GraphicsSP {
+
+	
+	public :
+		static int test;
+		static vector<Cart> cartsV;
+		static vector<Cart_item>cart_itemsV;
+		GraphicsSP();
+		static void display();
+	};
+
+
+#endif
